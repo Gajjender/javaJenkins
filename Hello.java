4891d8a1d727c0c76a06349887858a47db93625c
Gajjender <rathoregajjender2018@gmail.com>
2024-02-18 16:42:04 +0530

    second commit

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 class Hello{
     public static void main(String args[]){
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter your name : ");
-        String a =s.nextLine();
-        System.out.println("Your name is : "+a);
+ //       System.out.print("Enter your name : ");
+//        String a =s.nextLine();
+        System.out.println("Your name is : Gajjender Rathore");
     }
 }
