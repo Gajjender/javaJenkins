d3876e8b2369ad7df58fa2c58d8a77e72ab39e61
Gajjender <rathoregajjender2018@gmail.com>
2024-07-10 12:36:47 +0530

    Error is resolved now checking for revert mail for successful build.

--- Hello.java
+++ Hello.java
@@ -7,6 +7,6 @@ class Hello{
         System.out.println("Your name is : Gajjender Rathore");
         System.out.println("This sentence is used to check automatically build of job when anything changes in github.");
         System.out.println("This is another check for automatically build poll scm after code changes/pushes on github remote repo.");
-        System.out.println("This is for checking if build fails then email is sent to a recipient.);
+        System.out.println("This is for checking if build fails then email is sent to a recipient.");
     }
 }
